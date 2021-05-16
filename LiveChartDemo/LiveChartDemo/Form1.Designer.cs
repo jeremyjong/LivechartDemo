21773df02b465464f6b8e0545c896fec15d87882
Jeremy <jeremyjong@gmail.com>
2021-05-16 14:14:16 +0900

    main commit
    Signed-off-by: Jeremy <jeremyjong@gmail.com>

---
+++ LiveChartDemo/LiveChartDemo/Form1.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace LiveChartDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cartesianChart1 = new LiveCharts.WinForms.CartesianChart();
+            this.cartesianChart2 = new LiveCharts.WinForms.CartesianChart();
+            this.SuspendLayout();
+            // 
+            // cartesianChart1
+            // 
+            this.cartesianChart1.Location = new System.Drawing.Point(54, 13);
+            this.cartesianChart1.Name = "cartesianChart1";
+            this.cartesianChart1.Size = new System.Drawing.Size(455, 394);
+            this.cartesianChart1.TabIndex = 0;
+            this.cartesianChart1.Text = "cartesianChart1";
+            // 
+            // cartesianChart2
+            // 
+            this.cartesianChart2.Location = new System.Drawing.Point(603, 30);
+            this.cartesianChart2.Name = "cartesianChart2";
+            this.cartesianChart2.Size = new System.Drawing.Size(500, 376);
+            this.cartesianChart2.TabIndex = 1;
+            this.cartesianChart2.Text = "cartesianChart2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1130, 418);
+            this.Controls.Add(this.cartesianChart2);
+            this.Controls.Add(this.cartesianChart1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private LiveCharts.WinForms.CartesianChart cartesianChart1;
+        private LiveCharts.WinForms.CartesianChart cartesianChart2;
+    }
+}
+
---
+++ LiveChartDemo/LiveChartDemo/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using LiveCharts; //Core of the library
+using LiveCharts.Wpf; //The WPF controls
+using LiveCharts.WinForms; //the WinForm wrappers
+
+namespace LiveChartDemo
+{
+    public partial class Form1 : Form
+    {
+
+        private Random rand = new Random(0);
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // generate some random Y data
+            int pointCount = 50;
+            double[] ys1 = RandomWalk(pointCount);
+            double[] ys2 = RandomWalk(pointCount);
+            double[] ys3 = RandomWalk(pointCount);
+
+            // create series and populate them with data
+            var series1 = new LiveCharts.Wpf.ColumnSeries
+            {
+                Title = "Group A",
+                Values = new LiveCharts.ChartValues<double>(ys1)
+            };
+
+            var series2 = new LiveCharts.Wpf.ColumnSeries()
+            {
+                Title = "Group B",
+                Values = new LiveCharts.ChartValues<double>(ys2)
+            };
+
+            var series3 = new LiveCharts.Wpf.ColumnSeries()
+            {
+                Title = "Group C",
+                Values = new LiveCharts.ChartValues<double>(ys3)
+
+            };
+            
+
+            // display the series in the chart control
+            cartesianChart1.Series.Clear();
+            cartesianChart1.Series.Add(series1);
+            cartesianChart1.Series.Add(series2);
+            cartesianChart1.Series.Add(series3);
+            cartesianChart1.Zoom = LiveCharts.ZoomingOptions.Xy;
+
+
+
+            var series4 = new LiveCharts.Wpf.LineSeries
+            {
+                Title = "Group A",
+                Values = new LiveCharts.ChartValues<double>(ys1)
+            };
+
+            // display the series in the chart control
+            cartesianChart2.Series.Clear();
+            cartesianChart2.Series.Add(series4);
+            cartesianChart2.Zoom = LiveCharts.ZoomingOptions.Xy;
+
+
+        }
+
+        private double[] RandomWalk(int points = 5, double start = 100, double mult = 50)
+        {
+            // return an array of difting random numbers
+            double[] values = new double[points];
+            values[0] = start;
+            for (int i = 1; i < points; i++)
+                values[i] = values[i - 1] + (rand.NextDouble() - .5) * mult;
+            return values;
+        }
+
+
+    }
+            
+    
+}
